dc64e5034f97286cc4b85accb7cdf812edced7e3
Tommy Dinh <tommykdinh@csu.fullerton.edu>
2025-10-25 16:21:13 -0700

    up to date

--- src/MovieGrid.js
+++ src/MovieGrid.js
@@ -14,11 +14,11 @@ const MovieGrid = ({ movies }) => {
     <div className="movies-container">
       {movies.map(movie => (
         <div key={movie.id} className="movie-card">
-          {movie.poster_path ? (
-            <img src={imageUrl + movie.poster_path} alt={movie.title} />
-          ) : (
-            <div className="blank-poster"></div>
-          )}
+          <img 
+            src={movie.poster_path ? imageUrl + movie.poster_path : ''} 
+            alt={movie.title}
+            className={movie.poster_path ? '' : 'blank-poster'}
+          />
           <h3>{movie.title}</h3>
           <p>Release: {movie.release_date || 'N/A'}</p>
           <p>Rating: {movie.vote_average || 'N/A'}</p>
@@ -28,4 +28,4 @@ const MovieGrid = ({ movies }) => {
   );
 };
 
-export default MovieGrid; 
+export default MovieGrid;
